dbc3aa3f332f2e026517e8442eea853fdf665371
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2024-03-08 13:40:25 -0800

    connected instructor and course

--- server/index.js
+++ server/index.js
@@ -3,15 +3,20 @@ const express = require('express')
 require('dotenv').config();
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/schema')
+const colors=require('colors')
+const connectDB = require('./config/db')
+
 
 const port = process.env.PORT || 5000
 
 const app = express()
+
+connectDB()
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql:process.env.NODE_ENV === 'development'
 }))
 
-app.listen(port, console.log(`Server ${port}. postta çalışıyor`))
+app.listen(port, console.log(`Server ${port}. postta çalışıyor`.blue.underline))
 
